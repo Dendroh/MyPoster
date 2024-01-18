1d43df2528da198cb92429b8f56a831b4db77d4c
donghyeonyun <eatpaper1429@gmail.com>
2024-01-18 14:37:06 +0900

    CMS 통신시 예외처리 추가
    CMS 통신시 Sprite 로 변경하는 작업에서 에러가 발생할 시 무한 로딩이 걸리는 부분을 null 반환을 통해 로딩이 끝날 수 있도록 수정

--- Assets/PhotoTicket/02.Script/Configuration/HiddenButton.cs
+++ Assets/PhotoTicket/02.Script/Configuration/HiddenButton.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 public class HiddenButton : MonoBehaviour
 {
-    float count = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        DontDestroyOnLoad(this);
-        GetComponentInChildren<Button>().onClick.AddListener(() =>
-        {
-            count++;
-        });
-    }
+	float count = 0;
+	// Start is called before the first frame update
+	void Start()
+	{
+		DontDestroyOnLoad(this);
+		GetComponentInChildren<Button>().onClick.AddListener(() =>
+		{
+			count++;
+		});
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (count > 0)
-        {
-            count -= Time.deltaTime * 2;
-            if (count > 3) Application.Quit();
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if (count > 0)
+		{
+			count -= Time.deltaTime * 2;
+			if (count > 3) Application.Quit();
+		}
+	}
 }
